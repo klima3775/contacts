513e1ac95647025f9417607bbbb1e494228fa613
klima3775 <113065590+klima3775@users.noreply.github.com>
2024-03-22 02:01:47 +0200

    Удалены компоненты и стили контактов

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import ContactLister from "./App";
+import ContactList from "./components/сontactList/contactList";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ContactLister />
+    <ContactList />
   </React.StrictMode>
 );
